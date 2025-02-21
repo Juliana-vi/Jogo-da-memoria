a380d3b5824baf344b6dc81d79064c891ac8711c
Juliana-vi <j.vianna@escolar.ifrn.edu.br>
2025-02-20 23:52:46 -0300

    Def draw, Def check loss , def update, Def restart game, Def  start level, Def get video e fim do jogo

--- main.py
+++ main.py
@@ -163,6 +163,113 @@ class Tile(pygame.sprite.Sprite):
                         self.level = 1
                     self.generate_level(self.level)
 
+    def draw(self):
+        screen.fill(BLACK)
+
+        title_font = pygame.font.Font('fonts/font.ttf', 44)
+        content_font = pygame.font.Font('fonts/font.ttf', 24)
+
+        title_text = title_font.render('Jogo da Memória', True, WHITE)
+        title_rect = title_text.get_rect(midtop=(WINDOW_WIDTH // 2, 10))
+
+        level_text = content_font.render('Fase ' + str(self.level), True, WHITE)
+        level_rect = level_text.get_rect(midtop=(WINDOW_WIDTH // 2, 80))
+
+        info_text = content_font.render('Encontre as figurinhas semelhantes', True, BLACK)
+        info_rect = info_text.get_rect(midtop=(WINDOW_WIDTH // 2, 120))
+
+        if self.is_video_playing:
+            if self.success:
+                screen.blit(pygame.image.frombuffer(self.img.tobytes(), self.shape, 'BGR'), (0, 120))
+            else:
+                self.get_video()
+        else:
+            screen.blit(pygame.image.frombuffer(self.img.tobytes(), self.shape, 'BGR'), (0, 120))
+
+        if self.game_over:
+            game_over_text = content_font.render('Eita, você perdeu :( Aperte espaço para iniciar novamente', True, BLACK)
+            game_over_rect = game_over_text.get_rect(midbottom=(WINDOW_WIDTH // 2, WINDOW_HEIGHT - 40))
+            screen.blit(game_over_text, game_over_rect)
+
+        elif self.level_complete:
+            if self.level != 5:
+                next_text = content_font.render('Fase concluída. Aperte Espaço para próxima fase!', True, BLACK)
+            else:
+                next_text = content_font.render('Parabéns, você venceu!!! Aperte espaço para iniciar novamente', True, BLACK)
+            next_rect = next_text.get_rect(midbottom=(WINDOW_WIDTH // 2, WINDOW_HEIGHT - 40))
+            screen.blit(next_text, next_rect)
+
+        screen.blit(title_text, title_rect)
+        screen.blit(level_text, level_rect)
+        screen.blit(info_text, info_rect)
+        screen.blit(timer_text, timer_rect)
+        pygame.draw.rect(screen, BLACK, (WINDOW_WIDTH - 110, 0, 130, 70))
+        screen.blit(self.video_toggle, self.video_toggle_rect)
+        screen.blit(self.music_toggle, self.music_toggle_rect)
+
+        self.tiles_group.draw(screen)
+        self.tiles_group.update()
+
+    def check_loss(self):
+        if self.timer <= 0 or self.some_loss_condition:
+            self.game_over = True
+
+    def update(self, event_list):
+        if self.game_over:
+            for event in event_list:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    self.restart_game()
+            return
+        
+        self.timer -= 1 / 60
+        if self.timer <= 0:
+            self.game_over = True
+
+     def restart_game(self):
+        self.level = 1
+        self.game_over = False
+        self.timer = 60 
+        self.generate_level(self.level)
+
+    def start_level(self):
+        self.timer = 60
+
+    def get_video(self):
+        self.cap = cv2.VideoCapture('video/nuvem.mp4')
+        self.success, self.img = self.cap.read()
+        self.shape = self.img.shape[1::-1]
+
+pygame.init()
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 860
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Jogo da memoria')
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+game = Game()
+
+running = True
+while running:
+    event_list = pygame.event.get()
+    for event in event_list:
+        if event.type == pygame.QUIT:
+            running = False
+
+    game.update(event_list)
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
+
+
 
 
 
